37f16c94ebf2575dd3088131185c282244a193fb
luckybelcik <luckybelcik@gmail.com>
2025-07-25 16:22:52 +0200

    Updated Navbar to use flex instead finnicky margins between elements

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -44,12 +44,12 @@
   }
 </script>
 
-<div class="flex flex-1 bg-base-200">
+<div class="flex flex-row p-1 gap-1 items-center bg-base-200">
   <a
     href={page.url.pathname === '/editor' ? '/editor/settings' : '/editor'}
     aria-label="Open settings"
     tabindex="0"
-    class="btn m-1 btn-square btn-lg bg-base-300 hover:bg-base-200"
+    class="btn btn-square btn-lg bg-base-300 hover:bg-base-200"
   >
     <svg
       width="35px"
@@ -57,19 +57,19 @@
       class="fill-current"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg">
-    
+
       <path d="M4,13.743l-1,.579a1,1,0,0,0-.366,1.366l1.488,2.578a1,1,0,0,0,1.366.366L6.5,18.05a1.987,1.987,0,0,1,1.986,0l.02.011a1.989,1.989,0,0,1,1,1.724V21a1,1,0,0,0,1,1h3a1,1,0,0,0,1-1V19.782a1.985,1.985,0,0,1,.995-1.721l.021-.012a1.987,1.987,0,0,1,1.986,0l1.008.582a1,1,0,0,0,1.366-.366l1.488-2.578A1,1,0,0,0,21,14.322l-1-.579a1.994,1.994,0,0,1-1-1.733v-.021a1.991,1.991,0,0,1,1-1.732l1-.579a1,1,0,0,0,.366-1.366L19.876,5.734a1,1,0,0,0-1.366-.366L17.5,5.95a1.987,1.987,0,0,1-1.986,0L15.5,5.94a1.989,1.989,0,0,1-1-1.724V3a1,1,0,0,0-1-1h-3a1,1,0,0,0-1,1V4.294A1.856,1.856,0,0,1,8.57,5.9l-.153.088a1.855,1.855,0,0,1-1.853,0L5.49,5.368a1,1,0,0,0-1.366.366L2.636,8.312A1,1,0,0,0,3,9.678l1,.579A1.994,1.994,0,0,1,5,11.99v.021A1.991,1.991,0,0,1,4,13.743ZM12,9a3,3,0,1,1-3,3A3,3,0,0,1,12,9Z"/>
     </svg>
   </a>
 
-  <label for="file-upload-input-navbar" class="btn btn-square btn-lg overflow-hidden flex-shrink-0 m-1">
+  <label for="file-upload-input-navbar" class="btn btn-square btn-lg overflow-hidden flex-shrink-0">
     <img src={projectIconUrl()} alt="Project Icon Preview" />
   </label>
 
   {#if isEditingName}
     <input
       type="text"
-      class="input input-ghost mt-2 m-1 text-xl"
+      class="input input-ghost text-xl"
       bind:value={currentInputName}
       onblur={finishEditingName}
       onkeydown={handleKeydown}
@@ -79,7 +79,7 @@
   {:else}
     <button
         type="button"
-        class="btn btn-ghost bg-base-300 mt-2 m-1 text-xl p-2"
+        class="btn btn-ghost bg-base-300 text-xl p-2"
         onclick={startEditingName}
         aria-label="Project name: {$engineStore.projectData.name}. Click to edit."
     >
